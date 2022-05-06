7c288b54c719e32e076ff9c1bdc3dba36301f384
James Hodgkinson <james@terminaloutcomes.com>
2022-05-06 23:28:46 +1000

    closes #21 - updates click, adds testing and auto-merge

--- tests/test_click.py
+++ tests/test_click.py
@@ -6,6 +6,11 @@ from mqtt_monitor import cli
 def test_command_help() -> None:
     """ test that something works using click """
     runner = CliRunner()
-    result = runner.invoke(cli, ["--help"])
+    result = runner.invoke(
+        cli,
+        [
+            "--help",
+        ],
+        )
     assert result.exit_code == 0
     print(result)
